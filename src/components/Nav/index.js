45c9a37a783ceb52b01d3efe669a9cc1162a8f1b
samuel cordova <cordovas1337@gmail.com>
2022-07-10 18:37:07 -0500

    testing 9

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Nav({ currentPage, handlePageChange }) {
    return (
-      <nav class="navbar navbar-expand-lg bg-dark">
-         <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-dark">
+         <div className="container-fluid">
             <h1 className='text-light flex-grow-1 row name'>Eric Spencer</h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
+               <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
                   <li className='nav-item'>
                      <Link 
